f8af09eb67f67ee9c630645a61db29c707547269
madhan9876 <67366707+madhan9876@users.noreply.github.com>
2024-09-03 06:40:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Inprocess Quality Assurance", "Dispatch ", "Validation"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
